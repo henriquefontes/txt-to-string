24f5b5dad882f4f6d55facf7e813d7f66fd59df0
avsysgeo-henriquesilva <132364179+avsysgeo-henriquesilva@users.noreply.github.com>
2023-06-28 09:12:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function convertToString(_sql) {
+  const splittedSql = _sql.split("\n");
+
+  const sql = splittedSql
+    .map((line, index, array) => {
+      const isLastIndex = index === array.length - 1;
+
+      if (isLastIndex) {
+        return '"' + line + " " + '"';
+      }
+
+      return '"' + line + " " + "\\n" + '"' + "+" + "\n";
+    })
+    .join("");
+
+  return sql;
+}
+
+const sql = document.querySelector("textarea").value;
+const convertBtn = document.getElementById("convert");
+
+convertBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(convertToString(sql));
+});
